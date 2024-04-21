74b99e74df67982c73ec8a90f0a27796e06cf505
Yat Long Chan <yat3.chan@live.uwe.ac.uk>
2024-04-21 20:16:23 +0800

    complete bullet (2D rotation of vector)

--- Game/Bullet.cpp
+++ Game/Bullet.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "Bullet.h"
+#include "math.h"
 #include <string>
+#include <iostream>
 
 Bullet::Bullet(string _fileName, ID3D11Device* _pd3dDevice, IEffectFactory* _EF): CMOGO(_fileName, _pd3dDevice, _EF)
 {
@@ -10,7 +12,32 @@ Bullet::Bullet(string _fileName, ID3D11Device* _pd3dDevice, IEffectFactory* _EF)
 	gravity_enabled = true;
 }
 
-void Bullet::SetVelocity(Vector3 v3, Quaternion _q)
+void Bullet::SetVelocity(Vector3 v3, Vector3 r3)
 {
-	m_vel = (v3 * _q);
+	m_vel = RotateV3(v3, r3);
+	//m_vel = v3;
+}
+
+Vector3 Bullet::RotateV3(Vector3 v3, Vector3 r3)
+{
+	std::cout << '\n';
+	std::cout << v3.x << ' ' << v3.y << ' ' << v3.z << '\n';
+
+	v3 = Vector3(getMagnitude(v3) * cos(-r3.y + getAngleFromVector(v3)), v3.y, getMagnitude(v3) * sin(-r3.y + getAngleFromVector(v3)));
+
+	std::cout << r3.x << ' ' << r3.y << ' ' << r3.z << '\n';
+	std::cout << v3.x << ' ' << v3.y << ' ' << v3.z << '\n';
+
+	return v3;
+}
+
+float Bullet::getMagnitude(Vector3 v3)
+{
+	return sqrt(v3.x * v3.x + v3.z * v3.z);
+}
+
+//in radian
+float Bullet::getAngleFromVector(Vector3 v3)
+{
+	return atan(v3.z / v3.x);
 }
